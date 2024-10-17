b4d156d2cabcb148ffdf0f96d6eff29e242f4229
24shrouk <143081315+24shrouk@users.noreply.github.com>
2024-10-17 06:10:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
+from PIL import Image
+def main():
+    # Set the title of the app
 
-telecom.jpeg
+    st.title("Welcome to the Churn App")
+    image = Image.open('telecom.jpeg')
+
+# Display the image in Streamlit
+    st.image(image, use_column_width=True)
+
